ad65d254df7c657db99a034a93483bb7edbedff9
Filipe De Oliveira Góis <46633783825@info.local>
2023-12-09 17:45:31 -0300

    Novo

--- Projetos React/front_end_smart_fit/src/Components/Header/Header.jsx
+++ Projetos React/front_end_smart_fit/src/Components/Header/Header.jsx
@@ -1,11 +1,22 @@
 import React from 'react';
 import "./Header.css"
+import Logo from "../../Assets/images/logo.svg"
+import Container from '../Container/Container';
 
 const Header = () => {
     return (
-        <>
-            
-        </>
+        <header className="header-flex">
+            <Container>
+                <div className="header-logo">
+                    <img src={Logo} alt="Logo da empresa Smart-Fit" />
+                </div>
+
+                <div className="header-content">
+                    <h1>Reabertura smart fit</h1>
+                    <p>O horário de funcionamento das nossas unidades está seguindo os decretos de cada município. Por isso, verifique aqui se sua unidade está aberta e as medidas de segurança que estamos seguindo.</p>
+                </div>
+            </Container>
+        </header>
     );
 };
 
